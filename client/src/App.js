60a804a9d953b861f783f1de9d33d988fa52e4bc
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-29 01:28:46 +0600

    Доваил функционал корзине, для управления товаром

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,92 @@
+// // client/src/App.js
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Header from './components/Header/Header';
+// import Home from './components/Home';
+// import Register from './components/Register/Register';
+// import Login from './components/Login/Login';
+// import Profile from './components/Profile/Profile';
+// import Cart from './components/Cart/Cart';
+// import './App.css';
+//
+// function App() {
+//     const [cartItems, setCartItems] = useState([]);
+//
+//     const addToCart = (item) => {
+//         setCartItems((prevItems) => {
+//             const existingItem = prevItems.find((prevItem) => prevItem.id === item.id);
+//
+//             if (existingItem) {
+//                 return prevItems.map((prevItem) =>
+//                     prevItem.id === item.id ? { ...prevItem, quantity: prevItem.quantity + 1 } : prevItem
+//                 );
+//             } else {
+//                 return [...prevItems, { ...item, quantity: 1 }];
+//             }
+//         });
+//     };
+//
+//     const removeFromCart = (item) => {
+//         setCartItems((prevItems) => {
+//             const updatedItems = prevItems.filter((prevItem) => prevItem.id !== item.id);
+//             return updatedItems;
+//         });
+//     };
+//
+//     const clearCart = () => {
+//         setCartItems([]);
+//     };
+//
+//     const increaseQuantity = (item) => {
+//         setCartItems((prevItems) =>
+//             prevItems.map((prevItem) =>
+//                 prevItem.id === item.id ? { ...prevItem, quantity: prevItem.quantity + 1 } : prevItem
+//             )
+//         );
+//     };
+//
+//     const decreaseQuantity = (item) => {
+//         setCartItems((prevItems) =>
+//             prevItems.map((prevItem) =>
+//                 prevItem.id === item.id && prevItem.quantity > 1
+//                     ? { ...prevItem, quantity: prevItem.quantity - 1 }
+//                     : prevItem
+//             )
+//         );
+//     };
+//
+//     return (
+//         <Router>
+//             <Header />
+//             <Switch>
+//                 <Route exact path="/" render={(props) => <Home {...props} addToCart={addToCart} />} />
+//                 <Route path="/register" component={Register} />
+//                 <Route path="/login" component={Login} />
+//                 <Route path="/profile" component={Profile} />
+//                 <Route path="/cart">
+//                     <Cart
+//                         cartItems={cartItems}
+//                         removeFromCart={removeFromCart}
+//                         clearCart={clearCart}
+//                         increaseQuantity={increaseQuantity}
+//                         decreaseQuantity={decreaseQuantity}
+//                     />
+//                 </Route>
+//                 {/* TODO: Add more routes here */}
+//             </Switch>
+//         </Router>
+//     );
+// }
+//
+// export default App;
+
+
+
+
+
 // client/src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home';
 import Register from './components/Register/Register';
@@ -26,6 +112,26 @@ function App() {
         });
     };
 
+    const removeFromCart = (item) => {
+        setCartItems((prevItems) => {
+            const updatedItems = prevItems.filter((prevItem) => prevItem.id !== item.id);
+            return updatedItems;
+        });
+    };
+
+    const clearCart = () => {
+        setCartItems([]);
+    };
+
+    const updateQuantity = (item, newQuantity) => {
+        setCartItems((prevItems) => {
+            const updatedItems = prevItems.map((prevItem) =>
+                prevItem.id === item.id ? { ...prevItem, quantity: newQuantity } : prevItem
+            );
+            return updatedItems;
+        });
+    };
+
     return (
         <Router>
             <Header />
@@ -35,7 +141,16 @@ function App() {
                 <Route path="/login" component={Login} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/cart">
-                    <Cart cartItems={cartItems} />
+                    {cartItems.length === 0 ? (
+                        <Redirect to="/" />
+                    ) : (
+                        <Cart
+                            cartItems={cartItems}
+                            removeFromCart={removeFromCart}
+                            clearCart={clearCart}
+                            updateQuantity={updateQuantity}
+                        />
+                    )}
                 </Route>
                 {/* TODO: Add more routes here */}
             </Switch>
@@ -44,3 +159,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,18 +1,81 @@
+// // client/src/components/Cart/Cart.js
+// import React from 'react';
+// import './Cart.css';
+//
+// const Cart = ({ cartItems, removeFromCart, clearCart, increaseQuantity, decreaseQuantity }) => {
+//     const calculateTotal = () => {
+//         return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+//     };
+//
+//     return (
+//         <div className="cart-container">
+//             <h2>Shopping Cart</h2>
+//             {cartItems.length === 0 ? (
+//                 <p>Your cart is empty.</p>
+//             ) : (
+//                 <>
+//                     <div className="cart-items">
+//                         {cartItems.map((item) => (
+//                             <div key={item.id} className="cart-item">
+//                                 <img src={item.image} alt={item.name} />
+//                                 <div>
+//                                     <h3>{item.name}</h3>
+//                                     <p>Price: ${item.price.toFixed(2)}</p>
+//                                     <div className="quantity-buttons">
+//                                         <button onClick={() => decreaseQuantity(item)}>-</button>
+//                                         <p>{item.quantity}</p>
+//                                         <button onClick={() => increaseQuantity(item)}>+</button>
+//                                     </div>
+//                                     <button onClick={() => removeFromCart(item)}>Remove</button>
+//                                 </div>
+//                             </div>
+//                         ))}
+//                     </div>
+//                     <div className="cart-total">
+//                         <p>Total: ${calculateTotal()}</p>
+//                         <button onClick={clearCart}>Clear Cart</button>
+//                     </div>
+//                 </>
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default Cart;
+
+
+
+
+
 // client/src/components/Cart/Cart.js
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './Cart.css';
 
-const Cart = ({ cartItems }) => {
+const Cart = ({ cartItems, removeFromCart, clearCart, updateQuantity }) => {
+    const history = useHistory();
+
     const calculateTotal = () => {
-        // TODO: Implement logic to calculate the total price of items in the cart
         return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+    };
+
+    const handleCheckout = () => {
+        // TODO: Implement checkout logic (redirect, order processing, etc.)
+        // For now, just redirect to the home page after clearing the cart
+        clearCart();
+        history.push('/');
     };
 
     return (
         <div className="cart-container">
             <h2>Shopping Cart</h2>
             {cartItems.length === 0 ? (
-                <p>Your cart is empty.</p>
+                <div>
+                    <p>Your cart is empty.</p>
+                    <p>
+                        <Link to="/">Browse products</Link>
+                    </p>
+                </div>
             ) : (
                 <>
                     <div className="cart-items">
@@ -22,13 +85,23 @@ const Cart = ({ cartItems }) => {
                                 <div>
                                     <h3>{item.name}</h3>
                                     <p>Price: ${item.price.toFixed(2)}</p>
-                                    <p>Quantity: {item.quantity}</p>
+                                    <label htmlFor={`quantity-${item.id}`}>Quantity:</label>
+                                    <input
+                                        type="number"
+                                        id={`quantity-${item.id}`}
+                                        value={item.quantity}
+                                        onChange={(e) => updateQuantity(item, parseInt(e.target.value))}
+                                        min="1"
+                                    />
+                                    <button onClick={() => removeFromCart(item)}>Remove</button>
                                 </div>
                             </div>
                         ))}
                     </div>
                     <div className="cart-total">
                         <p>Total: ${calculateTotal()}</p>
+                        <button onClick={clearCart}>Clear Cart</button>
+                        <button onClick={handleCheckout}>Checkout</button>
                     </div>
                 </>
             )}
@@ -37,3 +110,9 @@ const Cart = ({ cartItems }) => {
 };
 
 export default Cart;
+
+
+
+
+
+
