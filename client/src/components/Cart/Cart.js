975b53b3896edc71d445967111372de64d728480
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-29 22:11:49 +0600

    Инпуты стали работать поотдельности и выводить общий счет

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -265,45 +265,112 @@
 
 
 
+// // client/src/components/Cart/Cart.js
+// import React from 'react';
+// import './Cart.css';
+//
+// const Cart = ({ cartItems, removeFromCart, clearCart, updateQuantity }) => {
+//     return (
+//         <div className="cart-container">
+//             <h2>Корзина</h2>
+//             {cartItems.map((item) => (
+//                 <div key={item._id} className="cart-item">
+//                     <div>
+//                         <img src={item.images[0].url} alt={`${item.brand} ${item.name}`} />
+//                     </div>
+//                     <div>
+//                         <p>{item.brand}</p>
+//                         <p>{item.name}</p>
+//                         <p>Цена: ${item.price.toFixed(2)}</p>
+//                     </div>
+//                     <div>
+//                         <input
+//                             type="number"
+//                             value={item.quantity}
+//                             onChange={(e) => updateQuantity(item, parseInt(e.target.value))}
+//                         />
+//                     </div>
+//                     <div>
+//                         <button onClick={() => removeFromCart(item)}>Убрать</button>
+//                     </div>
+//                 </div>
+//             ))}
+//             <div className="cart-summary">
+//                 <button onClick={clearCart}>Очистить корзину</button>
+//                 <p>Общая стоимость: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}</p>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default Cart;
+
+
+
+
+
 // client/src/components/Cart/Cart.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cart.css';
 
 const Cart = ({ cartItems, removeFromCart, clearCart, updateQuantity }) => {
+    const [cart, setCart] = useState({});
+
+    useEffect(() => {
+        const cartObject = {};
+        cartItems.forEach((item) => {
+            cartObject[item._id] = { item, quantity: item.quantity };
+        });
+        setCart(cartObject);
+    }, [cartItems]);
+
+    const handleUpdateQuantity = (item, newQuantity) => {
+        setCart((prevCart) => ({
+            ...prevCart,
+            [item._id]: { ...prevCart[item._id], quantity: newQuantity },
+        }));
+    };
+
     return (
         <div className="cart-container">
             <h2>Корзина</h2>
-            {cartItems.map((item) => (
-                <div key={item._id} className="cart-item">
+            {Object.values(cart).map((cartItem) => (
+                <div key={cartItem.item._id} className="cart-item">
                     <div>
-                        <img src={item.images[0].url} alt={`${item.brand} ${item.name}`} />
+                        <img src={cartItem.item.images[0].url} alt={`${cartItem.item.brand} ${cartItem.item.name}`} />
                     </div>
                     <div>
-                        <p>{item.brand}</p>
-                        <p>{item.name}</p>
-                        <p>Цена: ${item.price.toFixed(2)}</p>
+                        <p>{cartItem.item.brand}</p>
+                        <p>{cartItem.item.name}</p>
+                        <p>Цена: ${cartItem.item.price.toFixed(2)}</p>
                     </div>
                     <div>
                         <input
                             type="number"
-                            value={item.quantity}
-                            onChange={(e) => updateQuantity(item, parseInt(e.target.value))}
+                            value={cartItem.quantity}
+                            onChange={(e) => handleUpdateQuantity(cartItem.item, parseInt(e.target.value))}
                         />
                     </div>
                     <div>
-                        <button onClick={() => removeFromCart(item)}>Убрать</button>
+                        <button onClick={() => removeFromCart(cartItem.item)}>Убрать</button>
                     </div>
                 </div>
             ))}
             <div className="cart-summary">
                 <button onClick={clearCart}>Очистить корзину</button>
-                <p>Общая стоимость: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}</p>
+                <p>
+                    Общая стоимость: ${Object.values(cart).reduce(
+                    (total, cartItem) => total + cartItem.item.price * cartItem.quantity,
+                    0
+                ).toFixed(2)}
+                </p>
             </div>
         </div>
     );
 };
 
 export default Cart;
+
 
 
 
