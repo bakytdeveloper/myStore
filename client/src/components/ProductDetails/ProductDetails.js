f676591385b0b922da8231f546fb7c82bb355177
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-31 01:27:44 +0600

    Поправил расстановку картинок, на инфо странице

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -3,25 +3,32 @@
 //
 //
 // // client/src/components/ProductDetails/ProductDetails.js
-// import React, { useEffect, useState } from 'react';
+// import React, { useState, useEffect } from 'react';
 // import { useParams, useHistory } from 'react-router-dom';
 // import './ProductDetails.css';
 //
 // const ProductDetails = ({ products, addToCart }) => {
-//     const history = useHistory();
 //     const { productId } = useParams();
+//     const history = useHistory();
 //     const product = products.find((product) => product._id === productId);
 //
-//     // Получаем сохраненное значение из localStorage при загрузке компонента
 //     const [mainImage, setMainImage] = useState(
 //         JSON.parse(localStorage.getItem(`mainImage_${productId}`)) ||
 //         (product && product.images && product.images.length > 0 ? product.images[0] : null)
 //     );
 //
 //     useEffect(() => {
-//         // Сохраняем текущее значение mainImage в localStorage при его изменении
 //         localStorage.setItem(`mainImage_${productId}`, JSON.stringify(mainImage));
 //     }, [mainImage, productId]);
+//
+//     const handleThumbnailClick = (image) => {
+//         setMainImage(image);
+//     };
+//
+//     const handleAddToCart = () => {
+//         addToCart(product);
+//         // Добавьте здесь дополнительные действия, если необходимо
+//     };
 //
 //     if (!product) {
 //         return <div className="product-details">Product not found</div>;
@@ -29,61 +36,59 @@
 //
 //     const { brand, name, price, description, features, images } = product;
 //
-//     const handleThumbnailClick = (image) => {
-//         setMainImage(image);
-//     };
-//
-//     const handleAddToCart = () => {
-//         // Тут можно добавить функционал добавления в корзину
-//         addToCart(product);
-//     };
-//
 //     return (
 //         <div className="product-details">
 //             <div className="product-header">
-//                 <h2>{name}</h2>
-//                 <h3>{brand}</h3>
+//                 <div className="product-title">
+//                     <h2>{name}</h2>
+//                     <h3>{brand}</h3>
+//                 </div>
 //                 <button className="close-button" onClick={() => history.goBack()}>
 //                     &#10005;
 //                 </button>
 //             </div>
-//             <div className="product-images">
-//                 <div className="thumbnails">
-//                     {images &&
-//                     images.map((image, index) => (
-//                         <img
-//                             key={index}
-//                             src={image.url}
-//                             alt={`Thumbnail ${index}`}
-//                             className={`thumbnail ${mainImage === image ? 'active' : ''}`}
-//                             onClick={() => handleThumbnailClick(image)}
-//                         />
-//                     ))}
-//                 </div>
-//                 <div className="main-image">
-//                     <img src={mainImage ? mainImage.url : ''} alt="Main" />
-//                 </div>
-//             </div>
-//             <div className="product-info">
-//                 <p>Price: ${price.toFixed(2)}</p>
-//                 <p>Description: {description}</p>
-//                 <div className="product-features">
-//                     <p>Features:</p>
-//                     <ul>
-//                         {features &&
-//                         features.map((feature, index) => (
-//                             <li key={index}>
-//                                 {feature.key}: {feature.value}
-//                             </li>
+//             <div className="product-content">
+//                 <div className="product-images">
+//                     <div className="thumbnails">
+//                         {images &&
+//                         images.map((image, index) => (
+//                             <img
+//                                 key={index}
+//                                 src={image.url}
+//                                 alt={`Thumbnail ${index}`}
+//                                 className={`thumbnail ${mainImage === image ? 'active' : ''}`}
+//                                 onClick={() => handleThumbnailClick(image)}
+//                             />
 //                         ))}
-//                     </ul>
+//                     </div>
+//                     <div className="main-image">
+//                         <img src={mainImage ? mainImage.url : ''} alt="Main" />
+//                     </div>
+//                 </div>
+//                 <div className="product-info">
+//                     <div className="description-box">
+//                         <h4>Описание</h4>
+//                         <p>{description}</p>
+//                     </div>
+//                     <p className="price">Цена: ${price.toFixed(2)}</p>
+//                     <div className="product-features">
+//                         <h4>Характеристики:</h4>
+//                         <ul>
+//                             {features &&
+//                             features.map((feature, index) => (
+//                                 <li key={index}>
+//                                     {feature.key}: {feature.value}
+//                                 </li>
+//                             ))}
+//                         </ul>
+//                     </div>
 //                 </div>
 //             </div>
 //             <div className="product-actions">
 //                 <button className="add-to-cart-button" onClick={handleAddToCart}>
-//                     Add to Cart
+//                     Добавить в корзину
 //                 </button>
-//                 <button className="buy-now-button">Buy Now</button>
+//                 <button className="buy-now-button">Купить сейчас</button>
 //             </div>
 //         </div>
 //     );
@@ -94,14 +99,19 @@
 
 
 
+
+
+
+
+// client/src/components/ProductDetails/ProductDetails.js
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import './ProductDetails.css';
 
 const ProductDetails = ({ products, addToCart }) => {
     const { productId } = useParams();
-    const product = products.find((product) => product._id === productId);
     const history = useHistory();
+    const product = products.find((product) => product._id === productId);
 
     const [mainImage, setMainImage] = useState(
         JSON.parse(localStorage.getItem(`mainImage_${productId}`)) ||
@@ -112,26 +122,28 @@ const ProductDetails = ({ products, addToCart }) => {
         localStorage.setItem(`mainImage_${productId}`, JSON.stringify(mainImage));
     }, [mainImage, productId]);
 
-    if (!product) {
-        return <div className="product-details">Product not found</div>;
-    }
-
-    const { brand, name, price, description, features, images } = product;
-
     const handleThumbnailClick = (image) => {
         setMainImage(image);
     };
 
     const handleAddToCart = () => {
         addToCart(product);
-        // Дополнительные действия при добавлении в корзину, если необходимо
+        // Добавьте здесь дополнительные действия, если необходимо
     };
+
+    if (!product) {
+        return <div className="product-details">Product not found</div>;
+    }
+
+    const { brand, name, price, description, features, images } = product;
 
     return (
         <div className="product-details">
             <div className="product-header">
-                <h2>{name}</h2>
-                {/*<h3>{brand}</h3>*/}
+                <div className="product-title">
+                    <h2>{name}</h2>
+                    <h3>{brand}</h3>
+                </div>
                 <button className="close-button" onClick={() => history.goBack()}>
                     &#10005;
                 </button>
@@ -155,24 +167,27 @@ const ProductDetails = ({ products, addToCart }) => {
                     </div>
                 </div>
                 <div className="product-info">
-                    <p className="price">${price.toFixed(2)}</p>
-                    <p className="description">{description}</p>
+                    <div className="description-box">
+                        <h4>Описание</h4>
+                        <p>{description}</p>
+                        <p className="price">Цена: ${price.toFixed(2)}</p>
+                    </div>
+                    <div className="product-actions">
+                        <button className="add-to-cart-button" onClick={handleAddToCart}>
+                            Добавить в корзину
+                        </button>
+                        <button className="buy-now-button">Купить сейчас</button>
+                    </div>
                     <div className="product-features">
-                        <p className="features-title">Features:</p>
+                        <h4>Характеристики:</h4>
                         <ul>
                             {features &&
                             features.map((feature, index) => (
                                 <li key={index}>
-                                    <strong>{feature.key}:</strong> {feature.value}
+                                    {feature.key}: {feature.value}
                                 </li>
                             ))}
                         </ul>
-                    </div>
-                    <div className="product-actions">
-                        <button className="add-to-cart-button" onClick={handleAddToCart}>
-                            Add to Cart
-                        </button>
-                        <button className="buy-now-button">Buy Now</button>
                     </div>
                 </div>
             </div>
@@ -181,7 +196,3 @@ const ProductDetails = ({ products, addToCart }) => {
 };
 
 export default ProductDetails;
-
-
-
-
