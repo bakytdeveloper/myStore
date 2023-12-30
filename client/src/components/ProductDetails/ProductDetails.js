d7504a8c0b7f9c3e5d207e1f33089f38d7fde739
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-30 22:38:42 +0600

    Вы вел нужные данные, на страницу карточек

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -1,65 +1,89 @@
+
+
+//
+//
 // // client/src/components/ProductDetails/ProductDetails.js
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { useParams, useHistory } from 'react-router-dom';
 // import './ProductDetails.css';
 //
 // const ProductDetails = ({ products, addToCart }) => {
-//     const { productId } = useParams();
-//     const product = products.find((item) => item.id === parseInt(productId));
-//
-//     if (!product) {
-//         return <p>Product not found.</p>;
-//     }
-//
-//     return (
-//         <div className="product-details-container">
-//             <img src={product.image} alt={product.name} />
-//             <div>
-//                 <h2>{product.name} {product.name} </h2>
-//                 <p>{product.description}</p>
-//                 <p>Price: ${product.price.toFixed(2)}</p>
-//                 <button onClick={() => addToCart(product)}>Add to Cart</button>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default ProductDetails;
-
-
-
-// // client/src/components/ProductDetails/ProductDetails.js
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-//
-// const ProductDetails = ({ products }) => {
+//     const history = useHistory();
 //     const { productId } = useParams();
 //     const product = products.find((product) => product._id === productId);
 //
+//     // Получаем сохраненное значение из localStorage при загрузке компонента
+//     const [mainImage, setMainImage] = useState(
+//         JSON.parse(localStorage.getItem(`mainImage_${productId}`)) ||
+//         (product && product.images && product.images.length > 0 ? product.images[0] : null)
+//     );
+//
+//     useEffect(() => {
+//         // Сохраняем текущее значение mainImage в localStorage при его изменении
+//         localStorage.setItem(`mainImage_${productId}`, JSON.stringify(mainImage));
+//     }, [mainImage, productId]);
+//
 //     if (!product) {
-//         return <div>Product not found</div>;
+//         return <div className="product-details">Product not found</div>;
 //     }
 //
-//     const { brand, name, price, description, images } = product;
-//     console.log(images)
+//     const { brand, name, price, description, features, images } = product;
+//
+//     const handleThumbnailClick = (image) => {
+//         setMainImage(image);
+//     };
+//
+//     const handleAddToCart = () => {
+//         // Тут можно добавить функционал добавления в корзину
+//         addToCart(product);
+//     };
 //
 //     return (
-//         <div>
-//             <h2>{brand}</h2>
-//             <h3>{name}</h3>
-//             <p>Price: ${price.toFixed(2)}</p>
-//             <p>Description: {description}</p>
-//             <div>
-//                 {images && images.length > 0 && (
-//                     <div>
-//                         <p>Images:</p>
-//                         <div>
-//                             {images.map((image, index) => (
-//                                 <img key={index} src={image} alt={`Product ${index}`} />
-//                                 ))}
-//                         </div>
-//                     </div>
-//                 )}
+//         <div className="product-details">
+//             <div className="product-header">
+//                 <h2>{name}</h2>
+//                 <h3>{brand}</h3>
+//                 <button className="close-button" onClick={() => history.goBack()}>
+//                     &#10005;
+//                 </button>
+//             </div>
+//             <div className="product-images">
+//                 <div className="thumbnails">
+//                     {images &&
+//                     images.map((image, index) => (
+//                         <img
+//                             key={index}
+//                             src={image.url}
+//                             alt={`Thumbnail ${index}`}
+//                             className={`thumbnail ${mainImage === image ? 'active' : ''}`}
+//                             onClick={() => handleThumbnailClick(image)}
+//                         />
+//                     ))}
+//                 </div>
+//                 <div className="main-image">
+//                     <img src={mainImage ? mainImage.url : ''} alt="Main" />
+//                 </div>
+//             </div>
+//             <div className="product-info">
+//                 <p>Price: ${price.toFixed(2)}</p>
+//                 <p>Description: {description}</p>
+//                 <div className="product-features">
+//                     <p>Features:</p>
+//                     <ul>
+//                         {features &&
+//                         features.map((feature, index) => (
+//                             <li key={index}>
+//                                 {feature.key}: {feature.value}
+//                             </li>
+//                         ))}
+//                     </ul>
+//                 </div>
+//             </div>
+//             <div className="product-actions">
+//                 <button className="add-to-cart-button" onClick={handleAddToCart}>
+//                     Add to Cart
+//                 </button>
+//                 <button className="buy-now-button">Buy Now</button>
 //             </div>
 //         </div>
 //     );
@@ -70,41 +94,94 @@
 
 
 
-// client/src/components/ProductDetails/ProductDetails.js
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import './ProductDetails.css';
 
-const ProductDetails = ({ products }) => {
+const ProductDetails = ({ products, addToCart }) => {
     const { productId } = useParams();
     const product = products.find((product) => product._id === productId);
+    const history = useHistory();
+
+    const [mainImage, setMainImage] = useState(
+        JSON.parse(localStorage.getItem(`mainImage_${productId}`)) ||
+        (product && product.images && product.images.length > 0 ? product.images[0] : null)
+    );
+
+    useEffect(() => {
+        localStorage.setItem(`mainImage_${productId}`, JSON.stringify(mainImage));
+    }, [mainImage, productId]);
 
     if (!product) {
-        return <div>Product not found</div>;
+        return <div className="product-details">Product not found</div>;
     }
 
-    const { brand, name, price, description, images } = product;
+    const { brand, name, price, description, features, images } = product;
+
+    const handleThumbnailClick = (image) => {
+        setMainImage(image);
+    };
+
+    const handleAddToCart = () => {
+        addToCart(product);
+        // Дополнительные действия при добавлении в корзину, если необходимо
+    };
 
     return (
-        <div>
-            <h2>{brand}</h2>
-            <h3>{name}</h3>
-            <p>Price: ${price.toFixed(2)}</p>
-            <p>Description: {description}</p>
-            <div>
-                {images && images.length > 0 && (
-                    <div>
-                        <p>Images:</p>
-                        <div>
-                            {images.map((image, index) => (
-                                <img key={index} src={typeof image === 'string' ? image : image.url} alt={`Product ${index}`} />
-                            ))}
-                        </div>
+        <div className="product-details">
+            <div className="product-header">
+                <h2>{name}</h2>
+                {/*<h3>{brand}</h3>*/}
+                <button className="close-button" onClick={() => history.goBack()}>
+                    &#10005;
+                </button>
+            </div>
+            <div className="product-content">
+                <div className="product-images">
+                    <div className="thumbnails">
+                        {images &&
+                        images.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.url}
+                                alt={`Thumbnail ${index}`}
+                                className={`thumbnail ${mainImage === image ? 'active' : ''}`}
+                                onClick={() => handleThumbnailClick(image)}
+                            />
+                        ))}
                     </div>
-                )}
+                    <div className="main-image">
+                        <img src={mainImage ? mainImage.url : ''} alt="Main" />
+                    </div>
+                </div>
+                <div className="product-info">
+                    <p className="price">${price.toFixed(2)}</p>
+                    <p className="description">{description}</p>
+                    <div className="product-features">
+                        <p className="features-title">Features:</p>
+                        <ul>
+                            {features &&
+                            features.map((feature, index) => (
+                                <li key={index}>
+                                    <strong>{feature.key}:</strong> {feature.value}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="product-actions">
+                        <button className="add-to-cart-button" onClick={handleAddToCart}>
+                            Add to Cart
+                        </button>
+                        <button className="buy-now-button">Buy Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
 export default ProductDetails;
+
+
+
 
