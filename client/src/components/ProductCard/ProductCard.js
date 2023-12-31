5ddae79b802fcc9793bfd809dbff9b015ba36981
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-31 22:56:23 +0600

    Поправил функциона, по корректному открытию инфо страницы карточек

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -24,13 +24,43 @@
 
 
 
+// // client/src/components/ProductList/ProductCard.js
+// import React from 'react';
+// import './ProductCard.css';
+//
+// const ProductCard = ({ brand, name, price, images, addToCart }) => {
+//     return (
+//         <div className="product-card">
+//             {images && images.length > 0 && (
+//                 <img src={images[0].url} alt={`${brand} ${name}`} />
+//             )}
+//
+//             <h3>{brand}</h3>
+//             <h3>{name}</h3>
+//             <p>Цена: ${price.toFixed(2)}</p>
+//             <div className="b-button">
+//                 <button onClick={addToCart}>Корзина</button>
+//                 <button onClick={() => console.log('Купить')}>Купить</button>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default ProductCard;
+
+
+
 // client/src/components/ProductList/ProductCard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductCard.css';
 
-const ProductCard = ({ brand, name, price, images, addToCart }) => {
+const ProductCard = ({ product, addToCart }) => {
+    const { _id, brand, name, price, images } = product;
+
     return (
         <div className="product-card">
+            <Link to={`/product/${_id}`}>
             {images && images.length > 0 && (
                 <img src={images[0].url} alt={`${brand} ${name}`} />
             )}
@@ -38,15 +68,21 @@ const ProductCard = ({ brand, name, price, images, addToCart }) => {
             <h3>{brand}</h3>
             <h3>{name}</h3>
             <p>Цена: ${price.toFixed(2)}</p>
+            </Link>
             <div className="b-button">
                 <button onClick={addToCart}>Корзина</button>
-                <button onClick={() => console.log('Купить')}>Купить</button>
+
+                    <button onClick={() => console.log('Купить')}>Купить</button>
+
             </div>
         </div>
     );
 };
 
 export default ProductCard;
+
+
+
 
 
 
--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -26,9 +26,39 @@
 
 
 
+// // client/src/components/ProductList/ProductList.js
+// import React from 'react';
+// import { Link } from 'react-router-dom'; // Добавлено
+// import ProductCard from '../ProductCard/ProductCard';
+// import './ProductList.css';
+//
+// const ProductList = ({ products, addToCart }) => {
+//     return (
+//         <div className="product-list">
+//             {products.map((product) => (
+//                 // <Link key={product._id} to={`/product/${product._id}`}>
+//                 //      Обертываем каждую карточку в Link
+//                     <ProductCard
+//                         images={product.images}
+//                         brand={product.brand}
+//                         name={product.name}
+//                         price={product.price}
+//                         addToCart={() => addToCart(product)}
+//                     />
+//                 // </Link>
+//             ))}
+//         </div>
+//     );
+// };
+//
+// export default ProductList;
+
+
+
+
+
 // client/src/components/ProductList/ProductList.js
 import React from 'react';
-import { Link } from 'react-router-dom'; // Добавлено
 import ProductCard from '../ProductCard/ProductCard';
 import './ProductList.css';
 
@@ -36,16 +66,11 @@ const ProductList = ({ products, addToCart }) => {
     return (
         <div className="product-list">
             {products.map((product) => (
-                <Link key={product._id} to={`/product/${product._id}`}>
-                    {/* Обертываем каждую карточку в Link */}
-                    <ProductCard
-                        images={product.images}
-                        brand={product.brand}
-                        name={product.name}
-                        price={product.price}
-                        addToCart={() => addToCart(product)}
-                    />
-                </Link>
+                <ProductCard
+                    key={product._id}
+                    product={product}  // Передаем в ProductCard весь объект product
+                    addToCart={() => addToCart(product)}
+                />
             ))}
         </div>
     );
